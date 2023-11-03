58b5ba05f54b4fbdd26e46a353f76a59c119c6e0
paragkapoorcs <83000158+paragkapoorcs@users.noreply.github.com>
2023-11-03 13:07:21 +0530

    Update main.tf

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -15,3 +15,16 @@ resource "google_compute_firewall" "allow-http" {
   target_tags   = ["http-server2"]
   source_ranges = ["0.0.0.0/0"]
 }
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "${local.network}-allow-ssh"
+  network = "${local.network}"
+  project = "${var.project}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["http-server2"]
+  source_ranges = ["0.0.0.0/0"]
+}
